cd7858178b76fbc8c6009fa19696ec71fafc8d11
Harsh Jaiswal <harshjaiswal497@gmail.com>
2026-09-16 17:38:11 +0530

    Initial project and mongoDB setup

---
+++ src/main/java/com/movienotification/MovieNotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.movienotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieNotificationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieNotificationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/movienotification/MovieNotificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movienotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieNotificationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
